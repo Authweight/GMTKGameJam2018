1433fcd46ee465b04dbab6069c3ee253d4e627f7
Tom Goldthwait <tom@projectderailed.com>
2018-09-02 14:41:05 -0400

    Music

---
+++ GMTKGameJam2018/Assets/Scripts/Music.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public static bool loaded;
+	// Use this for initialization
+	void Start ()
+    {
+        if (loaded)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+        loaded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
